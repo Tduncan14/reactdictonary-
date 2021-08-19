7ea0fff0e14593dfb639fd20da57b968ee548ab9
tduncan14 <nbatre1@yahoo.com>
2021-08-18 20:26:00 -0700

    keep moving forward

--- dictonary/src/App.js
+++ dictonary/src/App.js
@@ -1,7 +1,31 @@
 
 import './App.css';
+import {useEfffect} from 'react';
 
 function App() {
+
+
+  const dictonaryApi = async () => {
+
+    try {
+
+      const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/<word>`)
+
+    }
+
+    catch(error){
+      console.log(error)
+    }
+  }
+
+
+  useEfffect(() =>{
+
+
+  },[])
+
+
+
   return (
     <div className="App">
       Hello
